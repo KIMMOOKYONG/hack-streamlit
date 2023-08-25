98f5e5f111bd9fd695ec04fba84adac0bc999749
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2023-08-25 19:50:22 +0900

    테스트

--- hands-on/app.py
+++ hands-on/app.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
-st.title("this is title")
-st.header("this is title")
-st.subheader("this is title")
+# Custom imports 
+from multipage import MultiPage
+from pages import data_upload, machine_learning, metadata, data_visualize, redundant, inference # import your pages here
+
+# Create an instance of the app 
+app = MultiPage()
+
+# Title of the main page
+st.title("Data Storyteller Application")
+
+# Add all your applications (pages) here
+app.add_page("Upload Data", data_upload.app)
+app.add_page("Change Metadata", metadata.app)
+app.add_page("Machine Learning", machine_learning.app)
+app.add_page("Data Analysis",data_visualize.app)
+app.add_page("Y-Parameter Optimization",redundant.app)
+
+# The main app
+app.run()
