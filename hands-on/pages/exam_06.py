6c5adf78c4165af5dc16502fa567a581480aabfc
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2023-08-25 22:01:02 +0900

    Update exam_06.py

--- hands-on/pages/exam_06.py
+++ hands-on/pages/exam_06.py
@@ -8,19 +8,28 @@ def app():
 
     df= pd.read_csv(iris_dataset)
     df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
-    df['species']= iris_dataset.target 
 
+    st.subheader("this is table")
+    st.table(df.head())
 
-    species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
-
-    def mapp_species(x):
-        return species_dict[x]
-
-    df['species'] = df['species'].apply(mapp_species)
-    print(df)
+    st.subheader("this is data frame")
+    st.dataframe(df.head())
 
     code = '''
+        import numpy as np
+        import pandas as pd 
+        import streamlit as st
 
+        iris_dataset = "https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv"
+
+        df= pd.read_csv(iris_dataset)
+        df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
+
+        st.subheader("this is table")
+        st.table(df.head())
+
+        st.subheader("this is data frame")
+        st.dataframe(df.head())
     '''
     st.code(code, language="python")
 
