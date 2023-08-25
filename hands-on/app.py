ed0854333d9a45299b6b80e33c5b3777ec62beb6
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2023-08-25 19:56:03 +0900

    테스트

--- hands-on/app.py
+++ hands-on/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Custom imports 
 from multipage import MultiPage
 # import your pages here
-from pages import exam-01
+from pages import exam_01
 
 # Create an instance of the app 
 app = MultiPage()
@@ -12,7 +12,7 @@ app = MultiPage()
 st.title("Data Storyteller Application")
 
 # Add all your applications (pages) here
-app.add_page("Upload Data", exam-01.app)
+app.add_page("Upload Data", exam_01.app)
 
 # The main app
 app.run()
