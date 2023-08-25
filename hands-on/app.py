219ee1343a676e59c10450a35774e619bbb79813
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2023-08-25 21:46:36 +0900

    테스트

--- hands-on/app.py
+++ hands-on/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Custom imports 
 from multipage import MultiPage
 # import your pages here
-from pages import exam_01, exam_02, exam_03, exam_04, exam_05
+from pages import exam_01, exam_02, exam_03, exam_04, exam_05, exam_06
 
 
 # Create an instance of the app 
@@ -18,6 +18,7 @@ app.add_page("layout 짜기-column", exam_02.app)
 app.add_page("layout 짜기-tab", exam_03.app)
 app.add_page("layout 짜기-sidebar", exam_04.app)
 app.add_page("이미지 불러오기", exam_05.app)
+app.add_page("웹사용자로부터 input 받기", exam_06.app)
 
 # The main app
 app.run()
---
+++ hands-on/pages/exam_06.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import streamlit as st
+
+def app():
+
+    iris_dataset = "https://raw.githubusercontent.com/pandas-dev/pandas/master/pandas/tests/data/iris.csv"
+
+    df= pd.read_csv(iris_dataset)
+    df.columns= [ col_name.split(' (cm)')[0] for col_name in df.columns] # 컬럼명을 뒤에 cm 제거하였습니다
+    df['species']= iris_dataset.target 
+
+
+    species_dict = {0 :'setosa', 1 :'versicolor', 2 :'virginica'} 
+
+    def mapp_species(x):
+        return species_dict[x]
+
+    df['species'] = df['species'].apply(mapp_species)
+    print(df)
+
+    code = '''
+
+    '''
+    st.code(code, language="python")
