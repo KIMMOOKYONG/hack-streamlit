f8ac12939ea598fac09dca33bf0994672e8c9bc6
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2023-08-25 21:17:02 +0900

    Update exam_05.py

--- hands-on/pages/exam_05.py
+++ hands-on/pages/exam_05.py
@@ -4,7 +4,9 @@ from PIL import Image
 def app():
     #PIL 패키지에 이미지 모듈을 통해 이미지 열기 
     # Image.open('이미지 경로')
-    zarathu_img = Image.open("https://github.com/KIMMOOKYONG/hack-streamlit/blob/main/hands-on/data/zarathu.jpg")
+    zarathu_img = Image.open("https://raw.githubusercontent.com/KIMMOOKYONG/hack-streamlit/main/hands-on/data/zarathu.jpg")
+
+st.image(htp5, caption= '80-day sale data', width=300)
 
     col1,col2 = st.columns([2,3])
     with col1 :
